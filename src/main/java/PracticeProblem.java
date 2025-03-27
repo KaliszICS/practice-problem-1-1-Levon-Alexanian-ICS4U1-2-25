b45e6418096a835e1f934958c5808c311a694095
Levon-Alexanian-ICS4U1-2-25 <levon.alexanian@student.tdsb.on.ca>
2025-03-27 05:10:23 +0000

    update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,28 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static int sum2D(int[][] array) {
+        int sum = 0;
+        for (int[] row : array) {
+            for (int num : row) {
+                sum += num;
+            }
+        }
+        return sum;
+    }
 
-	}
+	public static int sumRow(int[][] array, int row) {
+        int sum = 0;
+        for (int num : array[row]) {
+            sum += num;
+        }
+        return sum;
+    }
+
+	public static int sumColumn(int[][] array, int col) {
+        int sum = 0;
+        for (int[] row : array) {
+                sum += row[col];
+        }
+        return sum;
+    }
 }
